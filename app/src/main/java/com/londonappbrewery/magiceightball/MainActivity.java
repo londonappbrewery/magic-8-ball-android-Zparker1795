e563a2c8413332d7d203bdad4028f53305d66af3
Zparker1795 <Zparker56927@gmail.com>
2018-02-04 17:18:24 -0600

    Magic 8 Ball project

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,12 +2,47 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button myButton;
+        myButton = (Button) findViewById(R.id.myButton);
+
+        final ImageView balldisplay = (ImageView) findViewById(R.id.image_eightBall);
+
+        final int[] ballarray = {R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5,};
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("Magic 8 Ball", "You have pressed the button");
+
+                Random randomNumberGenerator = new Random();
+
+                int number = randomNumberGenerator.nextInt(5);
+
+                Log.d("Magic 8 Ball", "The Random Number is" + number);
+
+                balldisplay.setImageResource(ballarray[number]);
+
+            }
+
+        });
     }
 }
+
